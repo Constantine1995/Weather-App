90187b79ec8913d8281b4b53a45d5d2f237017c8
Kostya Likhachev <arrow.koss@gmail.com>
2019-05-27 14:35:07 +0300

    - created UI

---
+++ Weather-App/Weather-App/Extensions/CurrentWeather+Extension.swift
@@ -0,0 +1,28 @@
+//
+//  CurrentWeather-Extension.swift
+//  Weather-App
+//
+//  Created by mac on 5/27/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import Foundation
+
+extension CurrentWeather {
+    var pressureString: String {
+        return "\(Int(pressure)) mm"
+    }
+    
+    var humidityString: String {
+        return "\(Int(humidity)) %"
+    }
+    
+    var temperatureString: String {
+        return "\(Int(temperature)) ºC"
+    }
+    
+    var appearentTemperatureString: String {
+        return "\(Int(appearentTemperatur)) ºC"
+    }
+    
+}
---
+++ Weather-App/Weather-App/Extensions/WeatherIconManager+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherIconManager+Extension.swift
+//  Weather-App
+//
+//  Created by mac on 5/27/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import Foundation
